7f6d0cb15e4b2026299545662b6323acc28828ab
nikitazapekin <wotblitz362@mail.ru>
2024-12-10 14:56:04 +0300

    last

--- lab6/MainWindow.xaml.cs
+++ lab6/MainWindow.xaml.cs
@@ -8,6 +8,125 @@ namespace lab6
     {
         private CustomSet set1 = new CustomSet();
         private CustomSet set2 = new CustomSet();
+        private int[] set1Elements = new int[0]; // Инициализируем пустой массив
+        private int[] set2Elements = new int[0]; // Инициализируем пустой массив
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateDataGrids();
+        }
+
+        private void AddDefaultElements_Click(object sender, RoutedEventArgs e)
+        {
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set2.Add(2);
+            set2.Add(3);
+            set2.Add(4);
+
+            UpdateDataGrids();
+        }
+
+        private void UnionSets_Click(object sender, RoutedEventArgs e)
+        {
+            var unionSet = set1.Union(set2);
+            MessageBox.Show($"Объединение множеств: {unionSet}");
+        }
+
+        private void IntersectSets_Click(object sender, RoutedEventArgs e)
+        {
+            var intersectionSet = set1.Intersect(set2);
+            MessageBox.Show($"Пересечение множеств: {intersectionSet}");
+        }
+
+        private void DifferenceSets_Click(object sender, RoutedEventArgs e)
+        {
+            var differenceSet = set1.Difference(set2);
+            MessageBox.Show($"Разность множеств: {differenceSet}");
+        }
+
+        private void SortSet_Click(object sender, RoutedEventArgs e)
+        {
+            set1.Sort();
+            UpdateDataGrids();
+            MessageBox.Show($"Отсортированное множество 1: {set1}");
+        }
+
+        private void CompareElements_Click(object sender, RoutedEventArgs e)
+        {
+            if (set1.Count > 0 && set2.Count > 0)
+            {
+                var comparisonResult = set1[0].CompareTo(set2[0]);
+                string comparisonMessage = comparisonResult switch
+                {
+                    0 => "Элементы равны.",
+                    > 0 => "Первый элемент множества 1 больше первого элемента множества 2.",
+                    _ => "Первый элемент множества 1 меньше первого элемента множества 2."
+                };
+                MessageBox.Show(comparisonMessage);
+            }
+            else
+            {
+                MessageBox.Show("Оба множества должны содержать элементы.");
+            }
+        }
+
+        private void UpdateDataGrids()
+        {
+            set1Elements = set1.ToArray();
+            set2Elements = set2.ToArray();
+
+            Set1DataGrid.ItemsSource = null;
+            Set2DataGrid.ItemsSource = null;
+
+            Set1DataGrid.ItemsSource = set1Elements;
+            Set2DataGrid.ItemsSource = set2Elements;
+        }
+
+        // Обработчики для редактирования элементов в DataGrid
+        private void Set1DataGrid_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
+        {
+            if (e.EditAction == System.Windows.Controls.DataGridEditAction.Commit)
+            {
+                int editedValue;
+                if (int.TryParse(((System.Windows.Controls.TextBox)e.EditingElement).Text, out editedValue))
+                {
+                    set1.Remove((int)e.Row.Item);
+                    set1.Add(editedValue);
+                    UpdateDataGrids();
+                }
+            }
+        }
+
+        private void Set2DataGrid_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
+        {
+            if (e.EditAction == System.Windows.Controls.DataGridEditAction.Commit)
+            {
+                int editedValue;
+                if (int.TryParse(((System.Windows.Controls.TextBox)e.EditingElement).Text, out editedValue))
+                {
+                    set2.Remove((int)e.Row.Item);
+                    set2.Add(editedValue);
+                    UpdateDataGrids();
+                }
+            }
+        }
+    }
+}
+
+/*
+ * using System;
+using System.Windows;
+using System.Linq;
+
+namespace lab6
+{
+    public partial class MainWindow : Window
+    {
+        private CustomSet set1 = new CustomSet();
+        private CustomSet set2 = new CustomSet();
         private int[] set1Elements = Array.Empty<int>();
         private int[] set2Elements = Array.Empty<int>();
 
@@ -86,3 +205,4 @@ namespace lab6
         }
     }
 }
+*/
--- lab6/SetOperations.cs
+++ lab6/SetOperations.cs
@@ -1,4 +1,118 @@
 ﻿using System;
+
+namespace lab6
+{
+    public class CustomSet : IComparable
+    {
+        private int[] elements = new int[0]; // Use a simple array for dynamic resizing
+
+        public int Count => elements.Length;
+
+        public int this[int index] => elements[index];
+
+        public void Add(int element)
+        {
+            if (Array.IndexOf(elements, element) < 0) // Check if element already exists
+            {
+                Array.Resize(ref elements, elements.Length + 1);
+                elements[^1] = element; // Add the element at the end
+            }
+        }
+
+        public void Remove(int element)
+        {
+            int index = Array.IndexOf(elements, element);
+            if (index >= 0) // If the element exists
+            {
+                for (int i = index; i < elements.Length - 1; i++)
+                {
+                    elements[i] = elements[i + 1]; // Shift elements left
+                }
+                Array.Resize(ref elements, elements.Length - 1); // Resize array to remove the last element
+            }
+        }
+
+        public CustomSet Union(CustomSet other)
+        {
+            var result = new CustomSet();
+            foreach (var elem in elements)
+            {
+                result.Add(elem);
+            }
+            foreach (var elem in other.elements)
+            {
+                result.Add(elem);
+            }
+            return result;
+        }
+
+        public CustomSet Intersect(CustomSet other)
+        {
+            var result = new CustomSet();
+            foreach (var elem in elements)
+            {
+                if (Array.IndexOf(other.elements, elem) >= 0) // Check if element exists in the other set
+                {
+                    result.Add(elem);
+                }
+            }
+            return result;
+        }
+
+        public CustomSet Difference(CustomSet other)
+        {
+            var result = new CustomSet();
+            foreach (var elem in elements)
+            {
+                if (Array.IndexOf(other.elements, elem) < 0) // Element doesn't exist in the other set
+                {
+                    result.Add(elem);
+                }
+            }
+            return result;
+        }
+
+        public void Sort()
+        {
+            // Simple Bubble Sort for sorting the array
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[i] > elements[j])
+                    {
+                        int temp = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is CustomSet other)
+            {
+                return Count.CompareTo(other.Count);
+            }
+            throw new ArgumentException("Object is not a CustomSet");
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", elements);
+        }
+
+        public int[] ToArray()
+        {
+            return (int[])elements.Clone();
+        }
+    }
+}
+
+
+/*
+ * using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -106,11 +220,9 @@ namespace lab6
         }
 
 
-        /*   internal int[] ToArray()
-           {
-               throw new NotImplementedException();
-           } */
+        
     }
 }
 
 
+*/
