a0ecbdf2baca832bf1bc1bd307766e09fc5be1d2
nikitazapekin <wotblitz362@mail.ru>
2024-12-28 20:23:01 +0300

    last

--- lab9var12/Element.cs
+++ lab9var12/Element.cs
@@ -1,4 +1,96 @@
-﻿using System;
+﻿
+
+ 
+using System;
+
+
+public interface IInvertible
+{
+    void Invert();
+   
+}
+
+public abstract class Element : IInvertible
+
+{
+    private string name;
+    private int inputCount;
+    private int outputCount;
+
+
+
+
+    public virtual void SetInputs(int[] inputs)
+    {
+        throw new NotImplementedException("Ошибка");
+    }
+
+    public virtual void Invert()
+    {
+        throw new NotImplementedException("Ошибка");
+    }
+
+    public abstract int ComputeOutput();
+
+
+
+
+
+    public virtual string ToBinaryString()
+    {
+        throw new NotImplementedException();
+    }
+    public virtual void FromBinaryString(string dataString)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+
+    public Element(string name, int inputCount = 1, int outputCount = 1)
+    {
+        this.name = name;
+        this.inputCount = inputCount;
+        this.outputCount = outputCount;
+    }
+
+
+    public Element() { }
+
+
+    public string Name => name;
+    public int InputCount
+    {
+        get => inputCount;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Входы не могут быть отрицательными.");
+            inputCount = value;
+        }
+    }
+    public int OutputCount
+    {
+        get => outputCount;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Выходы не могут быть отрицательными.");
+            outputCount = value;
+        }
+    }
+
+
+
+   
+
+
+}
+
+
+/*
+ * using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,3 +102,4 @@ namespace lab9var12
     {
     }
 }
+*/
--- lab9var12/MainWindow.xaml.cs
+++ lab9var12/MainWindow.xaml.cs
@@ -15,14 +15,51 @@ using System.Windows.Shapes;
 
 namespace lab9var12
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Combinational combinationalElement;
+
         public MainWindow()
         {
             InitializeComponent();
+            combinationalElement = new Combinational(4); 
         }
+
+        private void ComputeOutput_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Обработчик вызван!");
+            OutputText.Text = "ысффсы фсыыс";
+           try
+            {
+             
+                int[] inputs = new int[4];
+                inputs[0] = ParseInput(Input1.Text, "Вход 1");
+                inputs[1] = ParseInput(Input2.Text, "Вход 2");
+                inputs[2] = ParseInput(Input3.Text, "Вход 3");
+                inputs[3] = ParseInput(Input4.Text, "Вход 4");
+ 
+                combinationalElement.SetInputs(inputs);
+                combinationalElement.SetInputs(inputs);
+                int result = combinationalElement.ComputeOutput();
+                OutputText.Text = $"Результат: {result}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        
+        }
+
+        private int ParseInput(string input, string fieldName)
+        {
+            if (!int.TryParse(input, out int value) || (value != 0 && value != 1))
+            {
+                throw new ArgumentException($"Поле \"{fieldName}\" должно содержать только 0 или 1.");
+            }
+            return value;
+        }
+
+
+
     }
 }
